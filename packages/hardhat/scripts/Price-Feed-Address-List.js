776f6fd1fb460b95cfd50774b5ce3dac7e3bca24
78carla <78c.carla@gmail.com>
2023-05-28 16:11:02 +0200

    Added Price-Feed-Address-List.js file

---
+++ packages/hardhat/scripts/Price-Feed-Address-List.js
@@ -0,0 +1,23 @@
+const PriceFeedAddressList = {
+  ethUsdPriceFeed: {
+    name: "ethUsd",
+    address: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    priceConsumerV3Address: "0xa2997F68ADa6E91a54040E98aecc2c612892EaCB",
+  },
+
+  btcUsdPriceFeed: {
+    name: "btcUsd",
+    address: "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43",
+    priceConsumerV3Address: "0x11bFA1d3020e58Ed0f7Ef93d012831FeFFdac790",
+  },
+
+  daiUsdPriceFeed: {
+    name: "daiUsd",
+    address: "0x14866185B1962B63C3Ea9E03Bc1da838bab34C19",
+    priceConsumerV3Address: "0x74a7f3ecb209d3353E61D24bC6Fa4Ab5B02C0375",
+  },
+};
+
+module.exports = {
+  PriceFeedAddressList,
+};
