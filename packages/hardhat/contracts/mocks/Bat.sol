e259c184322638ad9dcb328f93111b91feecb003
Angry Coding <mailidanielifechukwu@gmail.com>
2023-05-28 14:10:31 +0100

    Added mock tokens

---
+++ packages/hardhat/contracts/mocks/Bat.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Bat is ERC20 {
+    constructor() ERC20("BAT", "Brave Browser token") {}
+
+    function faucet(address to, uint amount) public {
+        _mint(to, amount);
+    }
+}
