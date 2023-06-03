aad469b547156c2dc327ba6dde68a37c818e568c
Angry Coding <mailidanielifechukwu@gmail.com>
2023-06-03 20:15:52 +0100

    Changes

--- packages/hardhat/helpers/contract-deployments.ts
+++ packages/hardhat/helpers/contract-deployments.ts
@@ -1,12 +1,12 @@
 import { MockV3Aggregator, Token, LinkToken } from "../typechain-types";
 import { deployContract } from "./utilities/tx";
 
-export const deployMockV3Aggregator = async (symbol: string, price: string) =>
+export const deployMockV3Aggregator = async (symbol: string, value: string, usdPriceFeed = true, id = "") =>
   await deployContract<MockV3Aggregator>(
     "MockV3Aggregator",
-    ["18", price],
+    ["18", value],
     undefined,
-    `${symbol}/USD PriceFeedAggregator`,
+    usdPriceFeed ? `${symbol}/USD-PriceFeedAggregator` : id,
   );
 
 export const deployMockToken = async (symbol: string, name: string) =>
